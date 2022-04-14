c7fda196364b7d7299498d6631a63ed18c60d445
CHH3213 <1213472983@qq.com>
2022-04-14 15:44:30 +0800

    上传数据

--- scripts/Data/compute_force.m
+++ scripts/Data/compute_force.m
@@ -2,7 +2,7 @@ clc;
 clear;
 % load('0109/3uav2/3droneData_v8.mat');
 
-load('20220414/2uav-9N/2droneSAData.mat');
+load('20220414/2uav-6N/2droneSAData.mat');
 thrust =(norm( sum(thrust_drone1(1000:1100,:),1)/100)^1.5+norm(sum(thrust_drone2(1000:1100,:),1)/100)^1.5)*4.328
 % thrust =(norm( sum(force_cable1(1000:1100,:),1)/100)^1.5+norm(sum(force_cable2(1000:1100,:),1)/100)^1.5)*4.328
 f = sum(force_cable1(1000:1100,:),1)/100+sum(force_cable2(1000:1100,:),1)/100
@@ -12,7 +12,7 @@ t = thrust/2
 
 
 % load('./3droneData_v8.mat');
-load('20220414/3uav-9N/3droneData_v8.mat')
+load('20220414/3uav-6N/3droneData_v8.mat')
 
 f1 = sum(force_cable1(1000:1100,:),1)/100+sum(force_cable2(1000:1100,:),1)/100+sum(force_cable3(1000:1100,:),1)/100
 thrust1 =(norm( sum(thrust_drone1(1000:1100,:),1)/100)^1.5+norm(sum(thrust_drone2(1000:1100,:),1)/100)^1.5+norm(sum(thrust_drone3(1000:1100,:),1)/100)^1.5)*4.328
